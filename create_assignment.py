4a0d540104b06f7ded073bfe9f033fff7cfe3355
WajeehJ <wajeehdoesstuff@gmail.com>
2025-08-27 14:03:59 -0500

    stuff

---
+++ create_assignment.py
@@ -0,0 +1,15 @@
+import docker
+
+client = docker.from_env()
+
+
+"""
+notes: 
+Make sure that Docker is running
+"""
+
+
+def create_assignment():
+    print("Hello World")
+
+    
---
+++ testing.py
@@ -0,0 +1,7 @@
+import create_assignment
+
+def main():
+    create_assignment.create_assignment() 
+
+if __name__ == "__main__":
+    main()
